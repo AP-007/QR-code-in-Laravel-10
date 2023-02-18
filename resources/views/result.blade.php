c29afc45aa47354e78d2bcc3752978d18de19a6b
AP-007 <poudel0070@gmail.com>
2023-02-18 19:39:27 +0545

    small update

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -6,7 +6,7 @@
         <div class="mb-8">
             {{ $result }}
         </div>
-        <div class="mb-8">
+        <div class="mb-8 flex justify-center">
             <a href="{{ route('qrcode') }}" class="bg-blue-600 hover:bg-blue-700 px-3 py-2 text-white rounded"> Generate Again
                 !!
             </a>
